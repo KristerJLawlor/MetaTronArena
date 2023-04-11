dee15c4d953a6d198d63425c6f0ab86d22848595
Krister Joseph Lawlor <kristerjlawlor@gmail.com>
2023-04-11 15:12:00 -0400

    AudioScript

--- Assets/MetaTronAssets/SFX/Scripts/GameMusicScript.cs
+++ Assets/MetaTronAssets/SFX/Scripts/GameMusicScript.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class GameMusicScript : MonoBehaviour
 {
+
+    public AudioSource GameMusic;
+
+    public AudioClip MenuClip;
+    public AudioClip ArenaClip;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameMusic.loop = true;
+        GameMusic.Play();
     }
 
     // Update is called once per frame
@@ -15,4 +22,5 @@ public class GameMusicScript : MonoBehaviour
     {
         
     }
+
 }
