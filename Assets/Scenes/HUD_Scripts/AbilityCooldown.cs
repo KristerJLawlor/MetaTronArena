4a25b3fe05fb36799ecbab6cd339dbe12583c4af
char1ro <93292604+char1ro@users.noreply.github.com>
2023-04-13 11:21:16 -0400

    UI

--- Assets/Scenes/HUD_Scripts/AbilityCooldown.cs
+++ Assets/Scenes/HUD_Scripts/AbilityCooldown.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AbilityCooldown : MonoBehaviour
 {
+    public Slider A_Ability;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    public void ChangeSliderValue(float cooldown)
+    {
+        A_Ability.value = cooldown;
+    }
+    public void ChangeSliderMaxValue(float CTime)
+    {
+        A_Ability.maxValue = CTime;
     }
 }
