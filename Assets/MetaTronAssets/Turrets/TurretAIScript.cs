d708c408b5ffd7248b8ef6e89ad9d3ee46e86ebc
KristerJLawlor <kristerjlawlor@gmail.com>
2023-04-17 03:07:41 -0400

    more turret AI updates (how do I test these)

--- Assets/MetaTronAssets/Turrets/TurretAIScript.cs
+++ Assets/MetaTronAssets/Turrets/TurretAIScript.cs
@@ -9,6 +9,20 @@ public class TurretAIScript : HighLevelEntity
     GameObject[] Players;
     GameObject Target;
     Vector3 TargetLoc;
+    public Transform LaserOrigin;
+    public float Range = 20f;
+    public float LaserDuration = 0.05f;
+    LineRenderer LaserLine;
+    public Vector3 AimPosition;
+    public Vector3 AimDirection;
+    public bool canShoot = true;
+    public RaycastHit hit;
+    public Vector3 SpawnLoc;
+    public bool TargetNear = false;
+
+    //Variables for particle effect
+    public GameObject LaserPrefab;
+    public GameObject LaserBeam;
 
 
 
@@ -38,6 +52,32 @@ public class TurretAIScript : HighLevelEntity
         {
             TargetLoc = ParseV3(value);
         }
+
+        if (IsClient && flag == "TARGETNEAR")
+        {
+            TargetNear = bool.Parse(value);
+        }
+
+
+        if (IsClient && flag == "CANSHOOT")
+        {
+            canShoot = bool.Parse(value);
+
+        }
+
+
+        if (IsServer && flag == "AP")
+        {
+            AimPosition = ParseV3(value);
+
+        }
+
+        if (IsServer && flag == "AD")
+        {
+            AimDirection = ParseV3(value);
+
+        }
+
     }
 
     public override void NetworkedStart()
@@ -47,10 +87,17 @@ public class TurretAIScript : HighLevelEntity
         {
             Players = GameObject.FindGameObjectsWithTag("Entity");
             OverShield = 0;
-            SendUpdate("SHIELD", OverShield.ToString());
             HP = 50;
-            SendUpdate("HP", HP.ToString());
         }
+    }
+
+    public IEnumerator ROF()
+    {
+
+            yield return new WaitForSeconds(1.5f);
+            canShoot = true;
+            SendUpdate("CANSHOOT", canShoot.ToString());
+
     }
 
     public override IEnumerator SlowUpdate()
@@ -64,16 +111,53 @@ public class TurretAIScript : HighLevelEntity
                 if ((transform.position - p.transform.position).magnitude < 20)
                 {
                     Target = p;
-                    
+                    TargetNear = true;
+                    SendUpdate("TARGETNEAR", TargetNear.ToString());
+                }
+                else
+                {
+                    TargetNear = false;
+                    SendUpdate("TARGETNEAR", TargetNear.ToString());
                 }
             }
             yield return new WaitForSeconds(.1f);
 
             SendUpdate("TARGET", Target.transform.position.ToString());
             this.transform.LookAt(Target.transform);
+
+            AimPosition = this.transform.position;
+            AimDirection = this.transform.forward;
+
+            if (canShoot && TargetNear)
+            {
+                //LaserLine.SetPosition(0, LaserOrigin.position);
+                if (Physics.Raycast(AimPosition, AimDirection, out hit, Range))
+                {
+                    if (hit.collider.tag == "Entity")
+                    {
+                        //LaserLine.SetPosition(1, hit.point);
+                        hit.transform.GetComponent<HighLevelEntity>().Damage(OnDamage(this.DamageScalar, hit.transform.gameObject), this.AProunds);
+                    }
+
+
+                }
+      
+                canShoot = false;
+                SendUpdate("CANSHOOT", canShoot.ToString());
+
+                StartCoroutine(ROF());
+
+            }
+
         }
 
     }
+
+    public virtual float OnDamage(float d, GameObject o)
+    {
+        return d;
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -84,6 +168,21 @@ public class TurretAIScript : HighLevelEntity
     // Update is called once per frame
     void Update()
     {
+        if(IsClient)
+        {
+            this.transform.LookAt(TargetLoc);
+
+            if(canShoot && TargetNear)
+            {
+                Destroy(LaserBeam);
+                LaserBeam = Instantiate(LaserPrefab, LaserOrigin.transform.position, transform.rotation);
+            }
+            else
+            {
+                Destroy(LaserBeam, 0.5f);
+            }
+
+        }
         
     }
 }
