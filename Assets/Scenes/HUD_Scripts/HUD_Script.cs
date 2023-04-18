7b8ad6c1b6cdb12148858b87d8a8c46ee1b6719f
char1ro <93292604+char1ro@users.noreply.github.com>
2023-04-18 14:18:58 -0400

    name

--- Assets/Scenes/HUD_Scripts/HUD_Script.cs
+++ Assets/Scenes/HUD_Scripts/HUD_Script.cs
@@ -94,7 +94,7 @@ public class HUD_Script : MonoBehaviour
 
         foreach (NetworkPlayerController npc in FindObjectsOfType<NetworkPlayerController>())
         {
-            ScoreList.Add(npc.name, npc.Score);
+            ScoreList.Add(npc.pname, npc.Score);
             //ScoreList[npc.name] = npc.Score;
         }
 
