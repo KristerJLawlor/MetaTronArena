8bf18d3fab65e1ec8d0a12ba572d06762b425e54
char1ro <93292604+char1ro@users.noreply.github.com>
2023-04-19 11:14:54 -0400

    Score

---
+++ Assets/Scenes/HUD_Scripts/ScoreUnit_Script.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreUnit_Script : MonoBehaviour
+{
+    public Text UserName;
+    public Text ScoreText;
+    public SortedList<string, int> ScoreList;
+    public int Element;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SBName(SortedList<string, int> List)
+    {
+        
+    }
+    public void SBScore(SortedList<string, int> Score)
+    {
+
+    }
+}
