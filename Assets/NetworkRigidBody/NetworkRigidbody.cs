56860fd6a8d2668b0a49978ad3360e26d69e8320
PCWilson12 <117094166+PCWilson12@users.noreply.github.com>
2024-03-20 10:53:48 -0400

    Undoing the revert I guess

--- Assets/NetworkRigidBody/NetworkRigidbody.cs
+++ Assets/NetworkRigidBody/NetworkRigidbody.cs
@@ -148,8 +148,11 @@ public class NetworkRigidbody : NetworkComponent
             {
                 myRig.velocity += AdaptiveVelocity;
             }
-            myRig.angularVelocity = LastAng;
-            myRig.rotation = Quaternion.Euler(LastRotation); 
+            if (!IsLocalPlayer)
+            {
+                myRig.angularVelocity = LastAng;
+                myRig.rotation = Quaternion.Euler(LastRotation);
+            }
         }
     }
 }
